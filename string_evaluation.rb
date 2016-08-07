2e20a13d00f944a8732e4e238293cda26f455756
sibaprasadsamantara <prasad.sibasamantara@gmail.com>
2016-08-07 14:19:50 +0530

    Minor modification

--- string_evaluation.rb
+++ string_evaluation.rb
@@ -1,25 +1,28 @@
 # Observation: How Ruby evaluates strings.
 
 # Ruby actually uses three different types of string values:
-# - Heap Strings - Asks extra memory from heap by calling malloc().
-# - Strings have same reference i.e str = "s", str1 = str
-# - Embedded Strings - New string
+# Heap Strings - Asks extra memory from heap by calling malloc().
+# Strings have same reference i.e str = "s", str1 = str
+# Embedded Strings - New string
 
-# Whenever we create a string value in Ruby, the interpreter goes through a below mentioned algorithm:
+# I use RVM and the Ruby source code is located under ~/.rvm/src
+# include/ruby/ruby.h which defines all of the basic Ruby data types and the string.c which explains Ruby String objects.
 
-1> Is this a new string value? Or a copy of an existing string? If it’s a copy, Ruby creates a String with the same referencec. This is the fastest option, since Ruby only needs a new RString structure.
+# Whenever we create a string value in Ruby, the interpreter goes through a similar to below mentioned algorithm:
 
-2> Is this a long string? Or a short string? If the new string value is 23 characters or less, Ruby creates an Embedded String and this is not as fast as a creating the string with same reference but it’s still fast because the 23 characters are simply copied right into the RString structure and there’s no need to call malloc.calling malloc is an expensive operation because it tracks the available memory and when to free them too.
+1> Is this a new string value? Or copy of an existing string? If it’s a copy, Ruby creates a String with the same referencec. This is the fastest option, since Ruby only needs a new RString structure.
+
+2> Is this a long string? Or short string? If the new string value is 23 characters or less, Ruby creates an Embedded String and this is not as fast as a creating the string with same reference but it’s still fast because the 23 characters are simply copied right into the RString structure and there’s no need to call malloc.calling malloc is an expensive operation because it tracks the available memory and when to free them too.
 
 3> Finally, for long string values, 24 characters or more, Ruby creates a Heap String - meaning it calls malloc and gets some new memory from the heap, and then copies the string value there. This is the slowest option and Ruby interpreter always allocates little more memory so that when you change anything to the string it should not call malloc() again.
 
-# The value of RSTRING_EMBED_LEN_MAX was chosen to match the size of the len/ptr/capa values. That’s where the 23 limit comes from.
-# Even worse, the value of RSTRING_EMBED_LEN_MAX for a 32-bit machine is less, in fact it's only 11. 
+# The value of RSTRING_EMBED_LEN_MAX for a 64-bit macine was chosen to match the size of the len/ptr/capa values i.e where the 23 limit comes from.
+# Even worse the value of RSTRING_EMBED_LEN_MAX for a 32-bit machine is less i.e only 11. 
 
 # IMPORTANT:
-# It's always a good idea to look into the Ruby C source code so that we can design the software based upon that which will give more performance.
+# It's always a good idea to look into the Ruby C source code so that we can design the better software.
 
-# CODE: Benchmarking Ruby 2.1.0 string allocation based upon "+", "concate", "<<"
+# CODE: Benchmarking Ruby 2.1.0 string allocation based upon methods called +, concate, <<
 
 require 'benchmark'
 
@@ -46,9 +49,9 @@ Benchmark.bmbm do |bm|
   run(str: "123456789012345678901234", bench: bm)
 end
 
-Case while creating new string by using 
+#Case while creating new string by using 
 
-"+"
+# +
 
 Rehearsal --------------------------------------------
 20 chars   0.280000   0.000000   0.280000 (  0.280640)
@@ -67,7 +70,7 @@ Rehearsal --------------------------------------------
 24 chars   0.420000   0.000000   0.420000 (  0.413061)
 25 chars   0.400000   0.000000   0.400000 (  0.406467)
 
-"concat"
+# concat
 
 Rehearsal --------------------------------------------
 20 chars   0.280000   0.000000   0.280000 (  0.274749)
@@ -86,7 +89,7 @@ Rehearsal --------------------------------------------
 24 chars   0.270000   0.000000   0.270000 (  0.272760)
 25 chars   0.270000   0.000000   0.270000 (  0.273072)
 
-"<<"
+# <<
 
 Rehearsal --------------------------------------------
 20 chars   0.250000   0.000000   0.250000 (  0.258534)
@@ -105,7 +108,6 @@ Rehearsal --------------------------------------------
 24 chars   0.260000   0.000000   0.260000 (  0.254474)
 25 chars   0.250000   0.000000   0.250000 (  0.253211)
 
-#Important 
-  
-  # "concat" and "<<" is faster then "+"
+# NOTE: 
+  # concat and << is faster then +
 
